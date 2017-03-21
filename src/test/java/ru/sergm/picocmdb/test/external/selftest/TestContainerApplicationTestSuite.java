0f2fba4de8e6f4cdc9c78873012aa7f3de45a1d9
Serge Martynov <serge.martynov@gmail.com>
2017-03-21 17:20:33 +0300

    Add Surefire plugin config.

--- src/test/java/ru/sergm/picocmdb/test/external/selftest/TestContainerApplicationTestSuite.java
+++ src/test/java/ru/sergm/picocmdb/test/external/selftest/TestContainerApplicationTestSuite.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TestContainerApplicationTests {
+public class TestContainerApplicationTestSuite {
 
 	@Test
 	public void home_Page_Opens() {
