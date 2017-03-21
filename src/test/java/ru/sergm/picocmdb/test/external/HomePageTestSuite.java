0c6ad2023faeb04ba5bf8e1b94109a63d370fbd4
Serge Martynov <serge.martynov@gmail.com>
2017-03-21 23:35:39 +0300

    Refactor pom.xml .

--- src/test/java/ru/sergm/picocmdb/test/external/HomePageTestSuite.java
+++ src/test/java/ru/sergm/picocmdb/test/external/HomePageTestSuite.java
@@ -7,6 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -14,7 +18,8 @@ public class HomePageTestSuite {
 
 	@Test
 	public void home_Page_Opens() {
-		assertEquals("", "");
+		open("http://tomcat.igelkott:8080/picocmdb/");
+		//assertEquals("", "");
 	}
 
 }
