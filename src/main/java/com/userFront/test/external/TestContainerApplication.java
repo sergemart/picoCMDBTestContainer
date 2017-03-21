0f026ab2ecd38596086499fdece1f91ccec5dae9
Serge Martynov <serge.martynov@gmail.com>
2017-03-21 14:45:42 +0300

    Initial commit.

---
+++ src/main/java/com/userFront/test/external/TestContainerApplication.java
@@ -0,0 +1,12 @@
+package com.userFront.test.external;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestContainerApplication.class, args);
+	}
+}
