bfe3f79124d1fc6aa0edc31b7c254588647bda82
Serge Martynov <serge.martynov@gmail.com>
2017-04-07 10:33:11 +0300

    Add create 'ManagedArea' REST controller test.

--- src/test/java/com/github/sergemart/picocmdb/test/external/restapi/atomic/ManagedAreaRestApiTests.java
+++ src/test/java/com/github/sergemart/picocmdb/test/external/restapi/atomic/ManagedAreaRestApiTests.java
@@ -1,14 +1,18 @@
 package com.github.sergemart.picocmdb.test.external.restapi.atomic;
 
-import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import org.junit.Test;
-
-import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.*;
+import io.restassured.http.ContentType;
 import static io.restassured.RestAssured.given;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.github.sergemart.picocmdb.test.external.AbstractTests;
 
@@ -16,7 +20,6 @@ import com.github.sergemart.picocmdb.test.external.AbstractTests;
 public class ManagedAreaRestApiTests extends AbstractTests {
 
 	private static final Logger LOG = LoggerFactory.getLogger(ManagedAreaRestApiTests.class);
-
 
 	@Test
 	public void test_Suite_Prerequisites_Initialized() {
@@ -57,7 +60,7 @@ public class ManagedAreaRestApiTests extends AbstractTests {
 		// GIVEN
 			// create entities, just in case if the database is empty
 		String entityName1 = "DUMMY" + super.getSalt();
-		super.jdbcTemplate.update("INSERT INTO managed_area(name, description) VALUES (?, 'тестовое описание')", (Object[]) new String[] {entityName1});
+		super.jdbcTemplate.update("INSERT INTO managed_area(name, description) VALUES (?, 'Тестовое описание.')", (Object[]) new String[] {entityName1});
 			// get auto-generated entity ID
 		String entityId1 = super.jdbcTemplate.queryForObject("SELECT id FROM managed_area WHERE (name = ?)", new String[] {entityName1}, String.class);
 		super.jdbcCleaner.addTask("DELETE FROM managed_area WHERE (name = ?)", new String[] {entityName1});
@@ -73,7 +76,7 @@ public class ManagedAreaRestApiTests extends AbstractTests {
 				and().
 				body( "id", equalTo(Integer.parseInt(entityId1)) ).				// check if API returns just created entity
 				body( "name", equalTo(entityName1) ).
-				body( "description", equalTo("тестовое описание") );		// check if UTF-8 chain is not broken
+				body( "description", equalTo("Тестовое описание.") );		// check if UTF-8 chain is not broken
 	}
 
 
@@ -94,5 +97,41 @@ public class ManagedAreaRestApiTests extends AbstractTests {
 				body( "localizedMessage", equalTo("Область управления не найдена.") ). // check if the language is switched
 				body( "errorCode", equalTo("1000404") );
 	}
+
+
+	@Test
+	public void service_Creates_ManagedArea() {
+		// GIVEN
+			// prepare ID and add task to delete created entity
+		String entityName1 = "DUMMY" + super.getSalt();
+		super.jdbcCleaner.addTask("DELETE FROM managed_area WHERE (name = ?)", new String[] {entityName1});
+			// construct JSON
+		Map<String, Object> jsonMap = new HashMap<>();
+		jsonMap.put("name", entityName1);
+		jsonMap.put("description", "Тестовое описание.");
+
+		Integer receivedEntityId1 =
+		given().
+				log().all().
+				body(jsonMap).
+				contentType(ContentType.JSON).
+		when().
+				post(super.baseRestUrl + "/managedareas/").
+		then().
+				log().all().
+				statusCode(200).								// check envelope
+				contentType(ContentType.JSON).
+				and().
+				body( "", hasKey("id") ).   									// check if API returns just created entity
+				body( "name", equalTo(entityName1) ).
+				body( "description", equalTo("Тестовое описание.") ).	// check if UTF-8 chain is not broken
+		 extract().
+				path("id");
+
+		// to check directly in database
+		Integer entityId1 = super.jdbcTemplate.queryForObject("SELECT id FROM managed_area WHERE (name = ?)", new String[] {entityName1}, Integer.class);
+		assertEquals(entityId1, receivedEntityId1);
+	}
+
 
 }
